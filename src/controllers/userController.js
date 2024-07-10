e958d84316ee33568be06fffad425757f2ae073a
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-10 19:46:04 +0700

    update response for userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,10 +3,17 @@ const userService = require('../services/userService');
 exports.registerUser = async (request, h) => {
   const newUser = await userService.registerUser(request.payload);
   if (newUser) {
-    return h.response(newUser).code(201).type('application/json');
+    return h
+      .response({
+        status: 'success',
+        message: 'User created',
+        data: { userId: newUser.id },
+      })
+      .code(201)
+      .type('application/json');
   }
   return h
-    .response({ message: 'Username already exist' })
+    .response({ status: 'fail', message: 'Username already exist' })
     .code(400)
     .type('application/json');
 };
@@ -14,26 +21,39 @@ exports.registerUser = async (request, h) => {
 exports.loginUser = async (request, h) => {
   const token = await userService.loginUser(request.payload);
   if (token) {
-    return h.response(token).code(200).type('application/json');
+    return h
+      .response({
+        status: 'success',
+        message: 'Login successful',
+        data: { token },
+      })
+      .code(200)
+      .type('application/json');
   }
   return h
-    .response({ message: 'Invalid username or password' })
+    .response({ status: 'fail', message: 'Invalid username or password' })
     .code(400)
     .type('application/json');
 };
 
 exports.getUsers = (request, h) => {
   const users = userService.getUsers();
-  return h.response(users).code(200).type('application/json');
+  return h
+    .response({ status: 'success', message: 'ok', data: { users } })
+    .code(200)
+    .type('application/json');
 };
 
 exports.getUserById = (request, h) => {
   const user = userService.getUserById(request.params.id);
   if (user) {
-    return h.response(user).code(200).type('application/json');
+    return h
+      .response({ status: 'success', message: 'ok', data: { user } })
+      .code(200)
+      .type('application/json');
   }
   return h
-    .response({ message: 'User not found' })
+    .response({ status: 'fail', message: 'User not found' })
     .code(404)
     .type('application/json');
 };
