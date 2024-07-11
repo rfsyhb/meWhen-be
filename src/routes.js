357493b315060fb5d4c8ebba87e346ec7f45356f
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-11 09:50:51 +0700

    update prettier

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,6 @@
 const userRoutes = require('./routes/userRoutes');
 const todoRoutes = require('./routes/todoRoutes');
 
-const routes = [
-  ...userRoutes,
-  ...todoRoutes,
-];
+const routes = [...userRoutes, ...todoRoutes];
 
 module.exports = routes;
