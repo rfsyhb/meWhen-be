cf81a0d6e72eba41f763a0db1d2c970b05a1a31c
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-13 12:11:21 +0700

    update exp token time to 24hrs

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -12,7 +12,8 @@ const generateToken = (user) => {
     name: user.name,
     instanceId: serverInstance.id,
     iat: Math.floor(Date.now() / 1000), // is used to determine the time at which a JWT was created
-    exp: Math.floor(Date.now() / 1000) + 60 * 60, // is used to determine the time at which a JWT becomes invalid (60 seconds * 60 minutes = 1 hour)
+    exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24, // updated to 24 hours (60 seconds * 60 minutes * 24 hours)
+    // exp: Math.floor(Date.now() / 1000) + 60 * 60, // is used to determine the time at which a JWT becomes invalid (60 seconds * 60 minutes = 1 hour)
   };
 
   return jwt.sign(payload, process.env.SECRET_KEY, { algorithm: 'HS256' });
