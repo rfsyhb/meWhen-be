e685a1434d69a963aa6fecfbf865999510ab065e
Rafi Syihab <rafisyihab3@gmail.com>
2024-07-19 17:17:08 +0700

    refactor user from in-memory array to integrated with supabase

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,60 +1,88 @@
 const userService = require('../services/userService');
 
 exports.registerUser = async (request, h) => {
-  const newUser = await userService.registerUser(request.payload);
-  if (newUser) {
+  try {
+    const newUser = await userService.registerUser(request.payload);
+    if (newUser) {
+      return h
+        .response({
+          status: 'success',
+          message: 'User registered successfully',
+          data: { userId: newUser.id },
+        })
+        .code(201)
+        .type('application/json');
+    }
     return h
-      .response({
-        status: 'success',
-        message: 'User created',
-        data: { userId: newUser.id },
-      })
-      .code(201)
+      .response({ status: 'fail', message: 'Username already exist' })
+      .code(400)
+      .type('application/json');
+  } catch (error) {
+    return h
+      .response({ status: 'error', message: error.message })
+      .code(500)
       .type('application/json');
   }
-  return h
-    .response({ status: 'fail', message: 'Username already exist' })
-    .code(400)
-    .type('application/json');
 };
 
 exports.loginUser = async (request, h) => {
-  const token = await userService.loginUser(request.payload);
-  if (token) {
+  try {
+    const token = await userService.loginUser(request.payload);
+    if (token) {
+      return h
+        .response({
+          status: 'success',
+          message: 'User logged in successfully',
+          data: { token },
+        })
+        .code(200)
+        .type('application/json');
+    }
     return h
-      .response({
-        status: 'success',
-        message: 'Login successful',
-        data: { token },
-      })
-      .code(200)
+      .response({ status: 'fail', message: 'Invalid username or password' })
+      .code(400)
+      .type('application/json');
+  } catch (error) {
+    return h
+      .response({ status: 'error', message: error.message })
+      .code(500)
       .type('application/json');
   }
-  return h
-    .response({ status: 'fail', message: 'Invalid username or password' })
-    .code(400)
-    .type('application/json');
 };
 
-exports.getUsers = (request, h) => {
-  const users = userService.getUsers();
-  return h
-    .response({ status: 'success', message: 'ok', data: { users } })
-    .code(200)
-    .type('application/json');
+exports.getUsers = async (request, h) => {
+  try {
+    const users = await userService.getUsers();
+    return h
+      .response({ status: 'success', message: 'ok', data: { users } })
+      .code(200)
+      .type('application/json');
+  } catch (error) {
+    return h
+      .response({ status: 'error', message: error.message })
+      .code(500)
+      .type('application/json');
+  }
 };
 
-exports.getOwnProfile = (request, h) => {
+exports.getOwnProfile = async (request, h) => {
   const { id: ownerId } = request.auth.credentials;
-  const user = userService.getOwnProfile(ownerId);
-  if (user) {
+  try {
+    const user = await userService.getOwnProfile(ownerId);
+    if (user) {
+      return h
+        .response({ status: 'success', message: 'ok', data: { user } })
+        .code(200)
+        .type('application/json');
+    }
     return h
-      .response({ status: 'success', message: 'ok', data: { user } })
-      .code(200)
+      .response({ status: 'fail', message: 'User not found' })
+      .code(404)
+      .type('application/json');
+  } catch (error) {
+    return h
+      .response({ status: 'error', message: error.message })
+      .code(500)
       .type('application/json');
   }
-  return h
-    .response({ status: 'fail', message: 'User not found' })
-    .code(404)
-    .type('application/json');
 };
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -6,52 +6,50 @@
 const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 const generateToken = require('../utils/generateToken');
+const supabase = require('../utils/supabaseClient');
 
 class User {
-  constructor() {
-    this.users = [
-      {
-        id: 'static-user-id-1',
-        name: 'Rafi Syihab',
-        username: 'rafi123',
-        password: bcrypt.hashSync('password123', 10),
-      },
-      {
-        id: 'static-user-id-2',
-        name: 'John Doe',
-        username: 'john123',
-        password: bcrypt.hashSync('password123', 10),
-      },
-    ];
-  }
-
-  async register(data) {
+  static async register(data) {
     const { name, username, password } = data; // Destructuring data
 
-    const isExist = this.users.find((user) => user.username === username);
-    if (isExist) {
+    const { data: existingUser } = await supabase
+      .from('users')
+      .select('*')
+      .eq('username', username)
+      .single();
+
+    // Check if username already exist
+    if (existingUser) {
       return null;
     }
 
     const id = nanoid(16); // Generate random id
     const hashedPassword = bcrypt.hashSync(password, 10); // Hashing password
 
-    // Create new user object
-    const newUser = {
-      id,
-      name,
-      username,
-      password: hashedPassword,
-    };
+    // Create new user
+    const { data: newUser, error: insertError } = await supabase
+      .from('users')
+      .insert([{ id, name, username, password: hashedPassword }])
+      .single()
+      .select();
 
-    this.users.push(newUser);
+    // catch error if exist
+    if (insertError) {
+      throw new Error(insertError.message);
+    }
+
     return newUser; // Return new user object after success registration
   }
 
-  async login(data) {
+  static async login(data) {
     const { username, password } = data; // Destructuring data
 
-    const user = this.users.find((userData) => userData.username === username);
+    const { data: user } = await supabase
+      .from('users')
+      .select('*')
+      .eq('username', username)
+      .single();
+
     if (!user) {
       return null;
     }
@@ -65,19 +63,31 @@ class User {
     return token; // Return token after success login
   }
 
-  getAll() {
-    return this.users.map(({ password, ...user }) => user); // Return all users without password
-  }
+  static async getAll() {
+    const { data: users, error } = await supabase
+      .from('users')
+      .select('id, name, username');
 
-  getOwnProfile(id) {
-    const user = this.users.find((userData) => userData.id === id);
-    if (user) {
-      const { password, ...filteredData } = user;
-      return filteredData; // Return user data without password
+    if (error) {
+      throw new Error(error.message);
     }
 
-    return null;
+    return users; // Return all users data
+  }
+
+  static async getOwnProfile(id) {
+    const { data: user, error } = await supabase
+      .from('users')
+      .select('id, name, username')
+      .eq('id', id)
+      .single();
+
+    if (error) {
+      return null;
+    }
+
+    return user;
   }
 }
 
-module.exports = new User();
+module.exports = User;
